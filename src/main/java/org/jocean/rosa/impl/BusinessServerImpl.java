e05c60be1291dd2dcd1fe3ea001e767bb5590ec1
ming.ma <ming.ma@upai.com>
2014-04-19 18:28:47 +0800

    支持 在 无参数并且返回值不为void的bean 方法上支持PathParam标注，目前查找Path占位符的优先级顺序为 1、字段 2、方法

--- src/main/java/org/jocean/rosa/impl/BusinessServerImpl.java
+++ src/main/java/org/jocean/rosa/impl/BusinessServerImpl.java
@@ -6,6 +6,7 @@ package org.jocean.rosa.impl;
 import io.netty.handler.codec.http.HttpRequest;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.util.HashMap;
@@ -16,9 +17,9 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 
 import org.jocean.event.api.EventReceiverSource;
-import org.jocean.idiom.PropertyPlaceholderHelper;
 import org.jocean.idiom.ExceptionUtils;
 import org.jocean.idiom.Function;
+import org.jocean.idiom.PropertyPlaceholderHelper;
 import org.jocean.idiom.ReflectUtils;
 import org.jocean.idiom.SimpleCache;
 import org.jocean.idiom.Visitor2;
@@ -120,19 +121,49 @@ public class BusinessServerImpl implements BusinessServerAgent {
 	        final Path path = reqCls.getAnnotation(Path.class);
 	        this._requestPath = null != path ? path.value() : null;
 	        this._queryFields = ReflectUtils.getAnnotationFieldsOf(reqCls, QueryParam.class);
-	        final Field[] pathparamFields = ReflectUtils.getAnnotationFieldsOf(reqCls, PathParam.class);
+	        
+	        this._pathparam2fields = genPath2FieldMapping(
+	                ReflectUtils.getAnnotationFieldsOf(reqCls, PathParam.class));
+	        
+            this._pathparam2methods = genPath2MethodMapping(reqCls, 
+                    ReflectUtils.getAnnotationMethodsOf(reqCls, PathParam.class));
+	        
+	    }
+
+        private Map<String, Field> genPath2FieldMapping(final Field[] pathparamFields) {
 	        if ( null != pathparamFields ) {
-	            this._pathparam2fields = new HashMap<String, Field>();
+	            final Map<String, Field> ret = new HashMap<String, Field>();
 	            for ( Field field : pathparamFields ) {
-	                this._pathparam2fields.put(
-	                        field.getAnnotation(PathParam.class).value(), field);
+	                ret.put(field.getAnnotation(PathParam.class).value(), field);
 	            }
+	            return ret;
 	        }
 	        else {
-	            this._pathparam2fields = null;
+	            return null;
 	        }
-	    }
+        }
 	    
+        private Map<String, Method> genPath2MethodMapping(final Class<?> reqCls, 
+                final Method[] pathparamMethods) {
+            if ( null != pathparamMethods ) {
+                final Map<String, Method> ret = new HashMap<String, Method>();
+                for ( Method method : pathparamMethods ) {
+                    if ( method.getParameterTypes().length == 0 
+                        && !method.getReturnType().equals(void.class)) {
+                        ret.put(method.getAnnotation(PathParam.class).value(), method);
+                    }
+                    else {
+                        LOG.warn("class({}).{} can't be invoke as PathParam, just ignore",
+                                reqCls, method.getName());
+                    }
+                }
+                return ( !ret.isEmpty() ?  ret : null);
+            }
+            else {
+                return null;
+            }
+        }
+        
         @Override
         public String apply(final Object request) {
             if ( null != this._pathparam2fields && null != this._requestPath ) {
@@ -182,6 +213,7 @@ public class BusinessServerImpl implements BusinessServerAgent {
 	    
         private final Field[] _queryFields;
         private final Map<String, Field> _pathparam2fields;
+        private final Map<String, Method> _pathparam2methods;
         private final String _requestPath;
         
         private final PropertyPlaceholderHelper _placeholderHelper = 
@@ -201,6 +233,18 @@ public class BusinessServerImpl implements BusinessServerAgent {
                                 request, field.getName(), ExceptionUtils.exception2detail(e));
                     }
                 }
+                
+                final Method method = _pathparam2methods.get(placeholderName);
+                if ( null != method ) {
+                    try {
+                        return String.valueOf( method.invoke(request) );
+                    }
+                    catch (Exception e) {
+                        LOG.error("exception when invoke ({}).{}, detail: {}", 
+                                request, method.getName(), ExceptionUtils.exception2detail(e));
+                    }
+                }
+                
                 // default by empty string, so placeholder will be erased from uri
                 return "";
             }};
