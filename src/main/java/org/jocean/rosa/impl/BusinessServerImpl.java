6b033f890b38e1b520e05cea40d9e954d68be147
ming.ma <ming.ma@upai.com>
2014-04-20 18:06:37 +0800

    fix typo for BusinessServerImpl.registerRequestType 重构 path param 特性支持，可替换 预注册的Request's Path String 中的 path param

--- src/main/java/org/jocean/rosa/impl/BusinessServerImpl.java
+++ src/main/java/org/jocean/rosa/impl/BusinessServerImpl.java
@@ -5,12 +5,14 @@ package org.jocean.rosa.impl;
 
 import io.netty.handler.codec.http.HttpRequest;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -20,6 +22,7 @@ import org.jocean.event.api.EventReceiverSource;
 import org.jocean.idiom.ExceptionUtils;
 import org.jocean.idiom.Function;
 import org.jocean.idiom.PropertyPlaceholderHelper;
+import org.jocean.idiom.PropertyPlaceholderHelper.PlaceholderResolver;
 import org.jocean.idiom.ReflectUtils;
 import org.jocean.idiom.SimpleCache;
 import org.jocean.idiom.Visitor2;
@@ -57,30 +60,27 @@ public class BusinessServerImpl implements BusinessServerAgent {
 		this._source = source;
 	}
 	
-	public BusinessServerImpl registerReuestType(final Class<?> reqCls, final String uri) {
-		this._req2uri.put(reqCls, uri);
+	public BusinessServerImpl registerRequestType(final Class<?> reqCls, final String pathPrefix) {
+		this._req2pathPrefix.put(reqCls, pathPrefix);
 		return this;
 	}
 	
-	private final HttpStack _stack;
+    private final HttpStack _stack;
 	private final EventReceiverSource _source;
-	private final Map<Class<?>, String> _req2uri = new HashMap<Class<?>, String>();
+	
+	private final Map<Class<?>, String> _req2pathPrefix = 
+	        new ConcurrentHashMap<Class<?>, String>();
+	
     private final SignalConverter _converter = new SignalConverter() {
 
         @Override
         public URI req2uri(final Object request) {
-            final RequestProcessor processor = _requestProcessorCache.get(
-                    request.getClass(), _cacheIfAbsent);
-            
-            final String suffix = processor.apply(request);
+            final String uri = 
+                _processorCache.get(request.getClass())
+                .apply(request);
             
             try {
-                if ( null == suffix ) {
-                    return new URI(_req2uri.get(request.getClass()));
-                }
-                else {
-                    return new URI(_req2uri.get(request.getClass()) + suffix);
-                }
+                return ( null != uri ? new URI(uri) : null);
             } catch (Exception e) {
                 LOG.error("exception when generate URI for request({}), detail:{}",
                         request, ExceptionUtils.exception2detail(e));
@@ -89,12 +89,12 @@ public class BusinessServerImpl implements BusinessServerAgent {
         }
 
         @Override
-        public HttpRequest processHttpRequest(final Object request, final HttpRequest httpRequest) {
-            final RequestProcessor processor = _requestProcessorCache.get(
-                    request.getClass(), _cacheIfAbsent);
-
+        public HttpRequest processHttpRequest(
+                final Object request, 
+                final HttpRequest httpRequest) {
             try {
-                processor.visit(request, httpRequest);
+                _processorCache.get(request.getClass())
+                    .visit(request, httpRequest);
             }
             catch (Exception e) {
                 LOG.error("exception when process httpRequest ({}) with request bean({})",
@@ -104,77 +104,46 @@ public class BusinessServerImpl implements BusinessServerAgent {
             return httpRequest;
         }};
         
-	private final SimpleCache<Class<?>, RequestProcessor> _requestProcessorCache = 
-	        new SimpleCache<Class<?>, RequestProcessor>();
-	
-    private final Function<Class<?>, RequestProcessor> _cacheIfAbsent = 
+	private final SimpleCache<Class<?>, RequestProcessor> _processorCache = 
+        new SimpleCache<Class<?>, RequestProcessor>(
             new Function<Class<?>, RequestProcessor>() {
                 @Override
                 public RequestProcessor apply(final Class<?> reqCls) {
                     return new RequestProcessor(reqCls);
-                }};
-    
-	private static final class RequestProcessor 
+                }});
+	
+	private final class RequestProcessor 
 	    implements Function<Object, String>, Visitor2<Object, HttpRequest> {
 
 	    RequestProcessor(final Class<?> reqCls) {
-	        final Path path = reqCls.getAnnotation(Path.class);
-	        this._requestPath = null != path ? path.value() : null;
-	        this._queryFields = ReflectUtils.getAnnotationFieldsOf(reqCls, QueryParam.class);
+            this._queryFields = ReflectUtils.getAnnotationFieldsOf(reqCls, QueryParam.class);
+            
+            this._pathSuffix = getPathValueOf(reqCls);
 	        
-	        this._pathparam2fields = genPath2FieldMapping(
-	                ReflectUtils.getAnnotationFieldsOf(reqCls, PathParam.class));
-	        
-            this._pathparam2methods = genPath2MethodMapping(reqCls, 
-                    ReflectUtils.getAnnotationMethodsOf(reqCls, PathParam.class));
-	        
+	        this._pathparamResolver = genPlaceholderResolverOf(reqCls, PathParam.class);
+
+	        this._pathparamReplacer = 
+	                ( null != this._pathparamResolver ? new PropertyPlaceholderHelper("{", "}") : null);
 	    }
 
-        private Map<String, Field> genPath2FieldMapping(final Field[] pathparamFields) {
-	        if ( null != pathparamFields ) {
-	            final Map<String, Field> ret = new HashMap<String, Field>();
-	            for ( Field field : pathparamFields ) {
-	                ret.put(field.getAnnotation(PathParam.class).value(), field);
-	            }
-	            return ret;
-	        }
-	        else {
-	            return null;
-	        }
-        }
-	    
-        private Map<String, Method> genPath2MethodMapping(final Class<?> reqCls, 
-                final Method[] pathparamMethods) {
-            if ( null != pathparamMethods ) {
-                final Map<String, Method> ret = new HashMap<String, Method>();
-                for ( Method method : pathparamMethods ) {
-                    if ( method.getParameterTypes().length == 0 
-                        && !method.getReturnType().equals(void.class)) {
-                        ret.put(method.getAnnotation(PathParam.class).value(), method);
-                    }
-                    else {
-                        LOG.warn("class({}).{} can't be invoke as PathParam, just ignore",
-                                reqCls, method.getName());
-                    }
-                }
-                return ( !ret.isEmpty() ?  ret : null);
-            }
-            else {
-                return null;
-            }
-        }
-        
         @Override
         public String apply(final Object request) {
-            if ( null != this._pathparam2fields && null != this._requestPath ) {
-                return this._placeholderHelper.replacePlaceholders(
+            final String pathPrefix = _req2pathPrefix.get(request.getClass());
+            if ( null == pathPrefix && null == this._pathSuffix ) {
+                // class not registered, return null
+                return null;
+            }
+            
+            final String fullPath = safeConcatPath(pathPrefix, this._pathSuffix );
+            if ( null != this._pathparamReplacer ) {
+                return this._pathparamReplacer.replacePlaceholders(
                         request,
-                        this._requestPath, 
-                        this._placeholderResolver, 
+                        fullPath, 
+                        this._pathparamResolver, 
                         null);
             }
             else {
-                return null;
+                return fullPath;
             }
         }
 	    
@@ -212,41 +181,123 @@ public class BusinessServerImpl implements BusinessServerAgent {
         }
 	    
         private final Field[] _queryFields;
-        private final Map<String, Field> _pathparam2fields;
-        private final Map<String, Method> _pathparam2methods;
-        private final String _requestPath;
         
-        private final PropertyPlaceholderHelper _placeholderHelper = 
-                new PropertyPlaceholderHelper("{", "}");
+        private final String _pathSuffix;
         
-        private final PropertyPlaceholderHelper.PlaceholderResolver _placeholderResolver = 
-                new PropertyPlaceholderHelper.PlaceholderResolver() {
-            @Override
-            public String resolvePlaceholder(final Object request, final String placeholderName) {
-                final Field field = _pathparam2fields.get(placeholderName);
-                if ( null != field ) {
-                    try {
-                        return String.valueOf( field.get(request) );
-                    }
-                    catch (Exception e) {
-                        LOG.error("exception when get value for ({}).{}, detail: {}", 
-                                request, field.getName(), ExceptionUtils.exception2detail(e));
-                    }
-                }
-                
-                final Method method = _pathparam2methods.get(placeholderName);
-                if ( null != method ) {
-                    try {
-                        return String.valueOf( method.invoke(request) );
-                    }
-                    catch (Exception e) {
-                        LOG.error("exception when invoke ({}).{}, detail: {}", 
-                                request, method.getName(), ExceptionUtils.exception2detail(e));
-                    }
-                }
-                
-                // default by empty string, so placeholder will be erased from uri
-                return "";
-            }};
+        private final PropertyPlaceholderHelper _pathparamReplacer;
+        
+        private final PlaceholderResolver _pathparamResolver;
 	};
+	
+    /**
+     * @param cls
+     */
+    private static String getPathValueOf(final Class<?> cls) {
+        final Path path = cls.getAnnotation(Path.class);
+        return (null != path ? path.value() : null);
+    }
+
+    private static String safeConcatPath(final String pathPrefix, final String pathSuffix) {
+        if ( null == pathSuffix ) {
+            return pathPrefix;
+        }
+        if ( null == pathPrefix ) {
+            return pathSuffix;
+        }
+        return pathPrefix + pathSuffix;
+    }
+
+    /**
+     * @return
+     */
+    private static PlaceholderResolver genPlaceholderResolverOf(
+            Class<?> cls, Class<? extends Annotation> annotationCls) {
+        final Map<String, Field> pathparam2fields = 
+            genPath2FieldMapping(
+                ReflectUtils.getAnnotationFieldsOf(cls, annotationCls));
+        
+        final Map<String, Method> pathparam2methods = 
+            genPath2MethodMapping(cls, 
+                ReflectUtils.getAnnotationMethodsOf(cls, annotationCls));
+        
+        if ( null == pathparam2fields 
+           && null ==  pathparam2methods) {
+            return null;
+        }
+        
+        return new PropertyPlaceholderHelper.PlaceholderResolver() {
+            @Override
+            public String resolvePlaceholder(final Object request,
+                    final String placeholderName) {
+                if (null != pathparam2fields) {
+                    final Field field = pathparam2fields
+                            .get(placeholderName);
+                    if (null != field) {
+                        try {
+                            return String.valueOf(field.get(request));
+                        } catch (Exception e) {
+                            LOG.error("exception when get value for ({}).{}, detail: {}",
+                                    request, field.getName(),
+                                    ExceptionUtils.exception2detail(e));
+                        }
+                    }
+                }
+
+                if ( null != pathparam2methods ) {
+                    final Method method = pathparam2methods
+                            .get(placeholderName);
+                    if (null != method) {
+                        try {
+                            return String.valueOf(method.invoke(request));
+                        } catch (Exception e) {
+                            LOG.error("exception when invoke ({}).{}, detail: {}",
+                                    request, method.getName(),
+                                    ExceptionUtils.exception2detail(e));
+                        }
+                    }
+                }
+
+                // default by empty string, so placeholder will be erased
+                // from uri
+                return "";
+            }
+        };
+    }
+    
+    private static Map<String, Field> genPath2FieldMapping(
+            final Field[] pathparamFields) {
+        if ( null != pathparamFields ) {
+            final Map<String, Field> ret = new HashMap<String, Field>();
+            for ( Field field : pathparamFields ) {
+                ret.put(field.getAnnotation(PathParam.class).value(), field);
+            }
+            return ret;
+        }
+        else {
+            return null;
+        }
+    }
+    
+    private static Map<String, Method> genPath2MethodMapping(
+            final Class<?> cls, 
+            final Method[] pathparamMethods) {
+        if ( null != pathparamMethods ) {
+            final Map<String, Method> ret = new HashMap<String, Method>();
+            for ( Method method : pathparamMethods ) {
+                if ( method.getParameterTypes().length == 0 
+                    && !method.getReturnType().equals(void.class)) {
+                    ret.put(method.getAnnotation(PathParam.class).value(), method);
+                }
+                else {
+                    LOG.warn("class({}).{} can't be invoke as PathParam, just ignore",
+                            cls, method.getName());
+                }
+            }
+            return ( !ret.isEmpty() ?  ret : null);
+        }
+        else {
+            return null;
+        }
+    }
+    
 }
