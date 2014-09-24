616c6ef5a13c6c9fcfae6347619948b07da2d076
ming.ma <isdom.maming@gmail.com>
2014-09-24 18:28:44 +0800

    移除过时的事件定义

--- src/main/java/org/jocean/httpclient/impl/FlowEvents.java
+++ src/main/java/org/jocean/httpclient/impl/FlowEvents.java
@@ -16,12 +16,6 @@ final class FlowEvents {
     //  from guide -> channel
     static final String RECOMMEND_CHANNEL_FOR_BINDING = "_recommend_channel_for_binding";
     
-//    //  from mediator -> guide
-//    static final String NOTIFY_GUIDE_FOR_CHANNEL_RESERVED = "_notify_guide_for_channel_reserved";
-    
-    //  from mediator -> guide
-//    static final String REQUEST_CHANNEL_PUBLISH_STATE = "_request_channel_publish_state";
-    
     //  from guide -> channel
     static final String REQUEST_CHANNEL_BIND_WITH_GUIDE = "_request_channel_bind_with_guide";
     
